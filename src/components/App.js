ec883a3dac18eabb74b395ca9be7dda70376b74d
Kal <kalvinsev@gmail.com>
2022-10-12 22:48:40 -0700

    Display grid/card flip functionality

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,63 @@ import './App.css';
 import MemoryToken from '../abis/MemoryToken.json'
 import brain from '../brain.png'
 
+const CARD_ARRAY = [
+  {
+    name: 'fries',
+    img: '/images/fries.png'
+  },
+  {
+    name: 'cheeseburger',
+    img: '/images/cheeseburger.png'
+  },
+  {
+    name: 'ice-cream',
+    img: '/images/ice-cream.png'
+  },
+  {
+    name: 'pizza',
+    img: '/images/pizza.png'
+  },
+  {
+    name: 'milkshake',
+    img: '/images/milkshake.png'
+  },
+  {
+    name: 'hotdog',
+    img: '/images/hotdog.png'
+  },
+  {
+    name: 'fries',
+    img: '/images/fries.png'
+  },
+  {
+    name: 'cheeseburger',
+    img: '/images/cheeseburger.png'
+  },
+  {
+    name: 'ice-cream',
+    img: '/images/ice-cream.png'
+  },
+  {
+    name: 'pizza',
+    img: '/images/pizza.png'
+  },
+  {
+    name: 'milkshake',
+    img: '/images/milkshake.png'
+  },
+  {
+    name: 'hotdog',
+    img: '/images/hotdog.png'
+  }
+]
+
 class App extends Component {
 
   async componentWillMount(){
     await this.loadWeb3()
     await this.loadBlockchainData()
+    this.setState({cardArray: CARD_ARRAY.sort(() => 0.5 - Math.random())})
   }
 
   async loadWeb3(){
@@ -53,13 +105,58 @@ class App extends Component {
     }
   }
 
+  chooseImage = (cardId) => {
+    cardId = cardId.toString()
+    if(this.state.cardsChosenId.includes(cardId)){
+      return CARD_ARRAY[cardId].img
+    } else {
+      return window.location.origin + '/images/blank.png'
+    }
+  }
+  flipCard = async (cardId) => {
+    let alreadyChosen = this.state.cardsChosen.length
+
+    this.setState({
+      cardsChosen: [...this.state.cardsChosen, this.state.cardArray[cardId].name],
+      cardsChosenId: [...this.state.cardsChosenId, cardId]
+    })
+
+    if (alreadyChosen === 1){
+      setTimeout(this.checkForMatch, 100)
+    }
+  }
+
+  checkForMatch = async () => {
+    const optionOneId = this.state.cardsChosenId[0]
+    const optionTwoId = this.state.cardsChosenId[1]
+
+    if(optionOneId== optionTwoId){
+      alert('You have clicked the same image!')
+    } else if(this.state.cardsChosen[0] === this.state.cardsChosen[1]){
+      alert('You found a match')
+    } else {
+      alert('Sorry, try again')
+    }
+    this.setState({
+      cardsChosen: [],
+      cardsChosenId: []
+    })
+    if(this.state.cardsWon.length === CARD_ARRAY.length){
+      alert('Congratulations! You found them all!')
+    }
+  }
+
   constructor(props) {
     super(props)
     this.state = {
       account: '0x0',
       token: null,
       totalSupply: 0,
-      tokenURIs: []
+      tokenURIs: [],
+      cardArray: [],
+      cardsChosen: [],
+      cardsChosenId: [],
+      cardsWon: []
     }
   }
 
@@ -90,7 +187,21 @@ class App extends Component {
 
                 <div className="grid mb-4" >
 
-                  {/* Code goes here... */}
+                  {this.state.cardArray.map((card, key) => {
+                    return(
+                      <img
+                        key={key}
+                        src={this.chooseImage(key)}
+                        data-id={key}
+                        onClick={(event) => {
+                          let cardId = event.target.getAttribute('data-id')
+                          if(!this.state.cardsWon.includes(cardId.toString())){
+                            this.flipCard(cardId)
+                          }
+                        }}
+                      />
+                    )
+                  })}
 
                 </div>
 
